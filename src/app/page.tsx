090db89c48b58af1a9360718c54c131e08e1c754
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-14 16:55:53 +0600

    added the responsive navbar using mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@
 
 export default function Home() {
   return (
-    <div className=" px-8">
-     
-      Home
+    <div className="text-center min-h-screen">
+   <h1 className="font-semibold text-6xl">
+   Write better <br/>
+content for your <br />
+  <u  className="bg-gradient-to-r from-yellow-400 to-red-600 text-transparent bg-clip-text">Facebook Ads </u>
 
+   </h1>
+   <p className="text-gray-400">Artificial intelligence writting tool helps you create blogs, <br /> social media websites and much more.</p>
     </div>
   );
 }
