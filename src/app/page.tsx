40a687be6b731b22724088383cb75af2314699a9
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-14 22:16:04 +0600

    banner section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-
+import Banner from '@/components/Banner';
 
 export default function Home() {
   return (
-    <div className="text-center min-h-screen">
-   <h1 className="font-semibold text-6xl">
-   Write better <br/>
-content for your <br />
-  <u  className="bg-gradient-to-r from-yellow-400 to-red-600 text-transparent bg-clip-text">Facebook Ads </u>
-
-   </h1>
-   <p className="text-gray-400">Artificial intelligence writting tool helps you create blogs, <br /> social media websites and much more.</p>
+    <div>
+      <Banner/>
     </div>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ButtonContained from '@/components/ButtonContained';
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+const Banner = () => {
+  return (
+    <div className="text-center min-h-screen my-8">
+   <h1 className="font-lg text-6xl">
+   Write better <br/>
+content for your <br />
+<span className="relative">
+  <span className="bg-gradient-to-r from-yellow-400 to-red-600 text-transparent bg-clip-text">
+    Facebook Ads
+  </span>
+  <span className="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-yellow-400 to-red-600"></span>
+</span>
+
+   </h1>
+   <p className="text-gray-400 my-6">Artificial intelligence writting tool helps you create blogs, <br /> social media websites and much more.</p>
+   <ButtonContained>{'Start 14 Days Free Trial'}</ButtonContained>
+   <div className='flex items-center gap-2 justify-center mt-4 text-sm' ><PlayCircleFilledIcon/> <span>Watch A Demo</span></div>
+    </div>
+  )
+}
+
+export default Banner
