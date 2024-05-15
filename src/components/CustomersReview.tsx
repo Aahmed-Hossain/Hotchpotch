bf51597e95a2ae051e174670f0b6b5a4a8cce1e1
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-15 12:57:08 +0600

    updated the mixlandhelp section due to responsive issue

---
+++ src/components/CustomersReview.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import darrellImg from '../../public/assests/Darrell Steward.png'
+import leslieImg from '../../public/assests/Leslie Alexander.png'
+import jennyImg from '../../public/assests/Jenny Wilson.png'
+import kristinImg from '../../public/assests/Kristin Watson.png'
+import guyImg from '../../public/assests/Guy Hawkins.png'
+import marvinImg from '../../public/assests/Marvin McKinney.png'
+import anneteImg from '../../public/assests/Annette Black.png'
+import floyedImg from '../../public/assests/Floyd Miles.png'
+const CustomersReview = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default CustomersReview
